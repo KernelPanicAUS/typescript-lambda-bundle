4814fea343710ed048f0913c2f7236982b4a5c34
Thomas Khalil <thomas@khalilt.com>
2019-12-25 23:32:43 +0100

    add aws-sdk

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,12 @@
-exports.handler = (event, callback) => {
-	console.info("hello from handler");
-	callback("hello world", null);
+import EC2 from "aws-sdk/clients/ec2";
+
+const client = new EC2({region: "eu-central-1"});
+
+exports.handler = async (event: any, callback: any) => {
+  console.info("hello from handler");
+  const ids = await client.describeVpcs()
+    .promise()
+    .then(r => r)
+    .catch(e => console.error(`Error occured: ${e.message}`));
+  callback("hello world", null);
 };
