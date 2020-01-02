32824cfdb0cadf9822f5c6a1a083cdb8e6a294d9
Thomas Khalil <thomas@khalilt.com>
2020-01-02 15:53:33 +0100

    rewrite lambda function

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,32 @@
+import { Callback, S3CreateEvent } from "aws-lambda";
 import EC2 from "aws-sdk/clients/ec2";
 
-const client = new EC2({region: "eu-central-1"});
+require("source-map-support").install({
+  environment: "node",
+});
+const client = new EC2({ region: "eu-central-1" });
 
-exports.handler = async (event: any, callback: any) => {
-  console.info("hello from handler");
-  const ids = await client.describeVpcs()
+// tslint:disable-next-line: interface-name
+interface Response {
+  statusCode: number;
+  body: string;
+}
+throw new Error("hello from the other side");
+exports.handler = async (event: S3CreateEvent, callback: Callback) => {
+  const ids: string[] | void = await client.describeVpcs()
     .promise()
-    .then(r => r)
-    .catch(e => console.error(`Error occured: ${e.message}`));
+    .then((r) => {
+      if (r.Vpcs === undefined) {
+        throw Error("Response did not contain any VPCs");
+      }
+      return r.Vpcs.map((v: EC2.Vpc) => v.VpcId ? v.VpcId : "");
+    })
+    .catch((e) => console.error(`Error occured: ${e.message}`));
+
+  const response: Response = {
+    body: ids ? ids[0] : "empty",
+    statusCode: 200,
+  };
+
   callback("hello world", null);
 };
